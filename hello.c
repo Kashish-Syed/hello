e2837d80aefdc0eaa4d9a35a6f88477ef8d3e139
Kashish Syed <ksyed2@huskers.unl.edu>
2022-08-25 18:48:20 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Kashish Syed
+ *         ksyed2@huskers.unl.edu
+ * Date: 2022/24/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  printf("Kashish Syed\n");
+
+  return 0;
+}
